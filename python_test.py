844c9096c5321d9582d8b41d2eaac30b718ef8ec
Sara Perry <paraserry@gmail.com>
2015-01-26 18:34:33 -0500

    more sorting logic

--- python_test.py
+++ python_test.py
@@ -1,15 +1,10 @@
 from sys import argv
 import re
 
-
-
-
 def read_from_file(file):
     #get the list from the file
     with open((file),'rb') as file:
         my_list = file.read().split(' ')
-
-
 
     clean_list_items(*my_list)
 
@@ -19,38 +14,52 @@ def clean_list_items(*args):
     for arg in args:
 
         #strip the element of weird characters and clean it
-        print "before clean %s" %arg
-        arg = arg.rstrip('\n')
-        print "after clean %s" %arg
-
+        #print "before clean %s" %arg
+        cleaned_arg = arg.rstrip('\n')
+        #print "after clean %s" %arg
 
         #and append it to the list
-        cleaned_list.append(int(arg) if arg.isdigit() else arg)
-    print cleaned_list
-
-    #print cleaned_list
-
+        cleaned_list.append(int(cleaned_arg) if arg.isdigit() else cleaned_arg)
 
     #call sort on the list
     sort_data(*cleaned_list)
 
 
 def sort_data(*args):
+
     #sort the data
+    ints_sorted = sorted([arg for arg in args if type(arg) == int])
+    strings_sorted = sorted([arg for arg in args if type(arg) == str])
+    sorted_data = []
 
+    for arg in args:
+        print "evaluating arg %s" %arg
 
-    #format the data
+        if isinstance(arg, int):
+            item = ints_sorted.pop(0)
+            print "popping item %s" %item
 
+            sorted_data.append(item)
+            print "item %s appended to sorted_list" %item
+
+        if isinstance(arg, str):
+            item = strings_sorted.pop(0)
+            print "popping item %s" %item
+            sorted_data.append(item)
+            print "item %s appended to sorted_list" %item
 
     #call write to file with the filename and the list
-    write_to_file('bananana file', *args)
+    write_to_file('sorted_file.txt', *sorted_data)
 
 
 def write_to_file(filename, *args):
-    pass
     #create a new file
+    with open(filename, 'w') as newfile:
+        for arg in args:
+            #write the data to the file
+            newfile.write("%s " % arg)
+        newfile.close()
 
-    #write the data to the file
 
 def main(filename):
     try:
@@ -59,8 +68,6 @@ def main(filename):
     except EOFError:
         print "bye"
         exit()
-
-
 
 if __name__ == "__main__":
     script, filename = argv
